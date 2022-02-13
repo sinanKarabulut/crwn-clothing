29732c1ef29eae2c04d1f49bb1e0a2a4a64b9177
sinan.karabulut <sinan.karabulut@akgun.com.tr>
2022-02-13 22:15:35 +0300

    homepage into smaller component,finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/HomePage/HomePage';
+import HomePage from './page/home-page/home-page';
 
 function App() {
   return (
